e9ac19b3079842c6d4e0b146770734333163252f
kuba-bujak <kuba.bujak0@wp.pl>
2022-03-24 15:25:46 +0100

    Moja aplikacja

--- app.py
+++ app.py
@@ -7,46 +7,37 @@ app = Flask(__name__)
 
 '''TRASOWANIE'''
 
-
 @app.route("/")
 def home():
     return render_template('index.html')
-
 
 @app.route("/aboutme")
 def aboutme():
     return render_template('aboutme.html')
 
-
 @app.route("/gallery")
 def gallery():
     return render_template('gallery.html')
-
 
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
 
-
 '''OBSLUGA BLEDOW'''
-
 
 @app.route('/error_denied')
 def error_denied():
     abort(401)
 
-
 @app.route('/error_internal')
 def error_internal():
     return render_template('template.html', name='ERROR 505'), 505
-
 
 @app.route('/error_not_found')
 def error_not_found():
     response = make_response(render_template('template.html', name='ERROR 404'), 404)
     response.headers['X-Something'] = 'A value'
     return response
-
 
 @app.errorhandler(404)
 def not_found_error(error):
