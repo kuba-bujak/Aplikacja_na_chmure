8c4b77e413cee5f1617209a2f58636421615e480
kuba-bujak <kuba.bujak0@wp.pl>
2022-05-05 00:57:47 +0200

    To moja nowa zmiana

--- app.py
+++ app.py
@@ -2,8 +2,19 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import abort, redirect, url_for, make_response
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
+'''E-mail'''
+mail= Mail(app)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
+app.config['MAIL_PASSWORD'] = '*****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 '''TRASOWANIE'''
 
@@ -22,10 +33,6 @@ def gallery():
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
-
-@app.route("/slider")
-def slider():
-    return render_template('slider.html')
 
 '''OBSLUGA BLEDOW'''
 
@@ -46,6 +53,8 @@ def error_not_found():
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
+
+
 
 
 if __name__ == '__main__':
