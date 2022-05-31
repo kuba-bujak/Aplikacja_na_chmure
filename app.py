bbc9c097fe15ac6b0f38557d36040c8c2c4ffdd3
vdi-terminal <kuba.bujak0@wp.pl>
2022-05-31 10:28:47 +0200

    Większa responsywność

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ from flask import abort, redirect, url_for, make_response
 from flask import Flask
 from flask_mail import Mail, Message
 
-
 app = Flask(__name__)
 
 ''' OBSŁUGA EMAIL'''
 
 mail = Mail(app)
-app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
 app.config['MAIL_PASSWORD'] = '*****'
@@ -21,31 +20,39 @@ app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 '''TRASOWANIE'''
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
+
 
 @app.route("/aboutme")
 def aboutme():
     return render_template('aboutme.html')
 
+
 @app.route("/gallery")
 def gallery():
     return render_template('gallery.html')
+
 
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
 
+
 '''OBSLUGA BLEDOW'''
+
 
 @app.route('/error_denied')
 def error_denied():
     abort(401)
 
+
 @app.route('/error_internal')
 def error_internal():
     return render_template('template.html', name='ERROR 505'), 505
+
 
 @app.route('/error_not_found')
 def error_not_found():
@@ -53,11 +60,10 @@ def error_not_found():
     response.headers['X-Something'] = 'A value'
     return response
 
+
 @app.errorhandler(404)
-def not_found_error(error):
+def not_found_error():
     return render_template('404.html'), 404
-
-
 
 
 if __name__ == '__main__':
